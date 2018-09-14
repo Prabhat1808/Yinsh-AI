54cdae6ef6228078892963a19d9ba587e1a586b3
Aniket Kumar <cs5160398@cse.iitd.ac.in>
2018-09-15 04:30:17 +0530

    random

--- game.cpp
+++ game.cpp
@@ -37,7 +37,6 @@ public:
     int my_marker;
     pair<int, int> beginning;
     pair<int, int> ending;
-
     Game(int player, int my_marker, int n=5, int rings=5, int max_row=5, int l=3){
         this-> n = n;
         this-> rings = rings;
@@ -85,13 +84,13 @@ public:
         this->rings_removed1 = rings1;
         this->rings_placed = rings_placed;
     }
-    Game copy_board(){
-        Game copied(player, my_marker, n, rings, max_row, l);
-        copied.update_state(this->state);
-        copied.update_rings(this->rings_removed0, this->rings_removed1, this->rings_placed);
+    Game* copy_board(){
+        Game* copied = new Game(player, my_marker, n, rings, max_row, l);
+        copied->update_state(this->state);
+        copied->update_rings(this->rings_removed0, this->rings_removed1, this->rings_placed);
         for(int i=0; i<board.size(); i++){
             for (int j=0; j<board.at(i).size(); j++){
-                copied.update_board(i, j, board.at(i).at(j)->data);
+                copied->update_board(i, j, board.at(i).at(j)->data);
             }
         }
         return copied;
