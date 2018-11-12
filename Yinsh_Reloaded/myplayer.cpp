9ec7a78e08c06e0ec4a5f357ad43fa9dd4243481
Prabhat1808 <prabhatkanaujia08@gmail.com>
2018-11-12 19:32:55 +0530

    Time steps added

--- Yinsh_Reloaded/myplayer.cpp
+++ Yinsh_Reloaded/myplayer.cpp
@@ -459,33 +459,99 @@ int main(int argc, char** argv)
    int ply=3;
    int t=0;
    int least_time;
-   if(n==5) least_time = 20;
-   else least_time = 50;
+   // if(n==5) least_time = 20;
+   // else least_time = 50;
    ofstream myfile;
    string filename = "logs_"+to_string(starting.at(0))+".txt";
    myfile.open(filename);
    double time_taken = 0;
    while(true){
 
+      if(n == 5)
+      {
+          if(ply == 4)
+          {
+              least_time = 20;
+          }
+          if(ply == 3)
+          {
+              least_time = 5;
+          }
+          if(ply == 2)
+          {
+              least_time = 3;
+          }
+      }
+      else
+      {
+          if(starting.at(3) == 6)
+          {
+              if(ply == 4)
+              {
+                  least_time = 80;
+              }
+              if(ply == 3)
+              {
+                  least_time = 10;
+              }
+              if(ply == 2)
+              {
+                  least_time = 5;
+              }
+          }
+          else
+          {
+              if(ply == 4)
+              {
+                  least_time = 50;
+              }
+              if(ply == 3)
+              {
+                  least_time = 8;
+              }
+              if(ply == 2)
+              {
+                  least_time = 4;
+              }
+          }
+      }
+
        int reward = 0;
        int prev_removed = game.self_removed();
-       pair<double, Game> inp = make_pair(game.heuristic(weights.at(ply-3)), game);
+       pair<double, Game> inp = make_pair(game.heuristic(weights.at(0)), game);
        pair<pair<int, Game>, vector<pair<int, int>>> taken = make_pair(inp, changed);
        clock_t t_start = clock();
-       pair<pair<int, Game>, string> mymove = maxval(starting.at(3), taken, INT_MIN, INT_MAX, ply, weights.at(ply-3), abselen, t_start, ply, total_time, time_taken, least_time);
+       pair<pair<int, Game>, string> mymove = maxval(starting.at(3), taken, INT_MIN, INT_MAX, ply, weights.at(0), abselen, t_start, ply, total_time, time_taken, least_time);
        // cerr << "Ply: "<< ply << endl;
        // cerr << endl << endl;
        cout << mymove.second << endl;
        game.execute_move(mymove.second);
        reward+=(game.self_removed() - prev_removed);
        t++;
+       if(n == 5)
+       {
+          if(t == 10)
+          {
+              ply = 4;
+          }
+       }
+       else
+       {
+            if(t == 15)
+            {
+                ply = 4;
+            }
+       }
        // cerr << ply << endl;
 
 
       time_taken += (double)(clock() - t_start)/CLOCKS_PER_SEC;
        // cerr << "Our time remaining: " << total_time - time_taken << endl;
-       if(total_time - time_taken < least_time) ply = 3;
-       else if(t==10) ply=4;
+       if(total_time - time_taken < least_time)
+       {
+            ply = max(ply-1,1);
+       }
+       // else if(t==10) ply=4;
         // if(t==20) ply = 5;
         // if(t==30) ply=6;
        getline(cin, move);
